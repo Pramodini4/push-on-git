77ec75e8994d03fe95f3968cda687ab4623237a2
Pramodini Dhavane <pramodinipdhavane4@gmail.com>
2025-07-02 09:03:03 +0530

    My First maven project

---
+++ src/main/java/com/pushongit/push_on_git/PushOnGitApplication.java
@@ -0,0 +1,13 @@
+package com.pushongit.push_on_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushOnGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PushOnGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pushongit/push_on_git/PushOnGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pushongit.push_on_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushOnGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
